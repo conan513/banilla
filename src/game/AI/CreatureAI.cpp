5bc4313d6e2131f40942f8a194fc8004124bbe4b
Prithu <prithu@iinet.net.au>
2017-05-11 07:31:02 +1000

    Prevent NPC backstab spells from being castable while the target is facing the creature (#671)

--- src/game/AI/CreatureAI.cpp
+++ src/game/AI/CreatureAI.cpp
@@ -61,6 +61,9 @@ CanCastResult CreatureAI::CanCastSpell(Unit* pTarget, const SpellEntry *pSpell, 
 		     return CAST_FAIL_NOT_IN_LOS;
     }
 
+    if (pSpell->Custom & SPELL_CUSTOM_FROM_BEHIND && pTarget->HasInArc(M_PI_F, m_creature))
+        return CAST_FAIL_OTHER;
+    
     if (pSpell->rangeIndex == SPELL_RANGE_IDX_SELF_ONLY)
         return CAST_OK;
 
